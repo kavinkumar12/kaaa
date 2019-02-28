6ebb8edf545c80fe6373479b0f94c6e61b405da6
kavinkumar12 <42169917+kavinkumar12@users.noreply.github.com>
2019-02-28 11:06:29 +0530

    Create kk2.py

---
+++ kk2.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number:"))
+flag=num%2
+if flag == 0:
+    print(number,"is an even number")
+elif flag == 1:
+    print(number,"is an odd number")
+else:
+    print(Error,"Invalid input")
